3a074f17d1eb33e9bdbd7c486a1def2e2fdc8759
kaiwen <skyddyyu@outlook.com>
2024-06-05 23:55:58 +0800

    :art: 增加tailwind工具函数cn的注释

--- src/lib/utils/tailwind.ts
+++ src/lib/utils/tailwind.ts
@@ -1,6 +1,12 @@
 import {ClassValue, clsx} from "clsx";
 import {twMerge} from "tailwind-merge";
 
+/**
+ * 用于合并tailwind类名
+ * 补充：合并时，后面的会覆盖前面的，以此保证覆盖性和优先级。
+ *
+ * @param classes
+ */
 export function cn(...classes: ClassValue[]) {
     return twMerge(clsx(classes))
 }
